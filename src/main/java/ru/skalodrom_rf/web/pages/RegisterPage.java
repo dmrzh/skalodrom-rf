d2bcccd751079c8e44f566ab96668a22199d9d32
Rzhevskiy Dmitry <dima@rzhevskiy.info>
2011-01-29 20:25:39 +0300

    added captch , fixed security with reminder.html, added login error message

--- src/main/java/ru/skalodrom_rf/web/pages/RegisterPage.java
+++ src/main/java/ru/skalodrom_rf/web/pages/RegisterPage.java
@@ -4,10 +4,15 @@ import org.apache.wicket.extensions.markup.html.captcha.CaptchaImageResource;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import ru.skalodrom_rf.dao.UserDao;
 import ru.skalodrom_rf.model.TUser;
@@ -16,34 +21,63 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class RegisterPage extends WebPage{
-     private final CaptchaImageResource captchaImageResource;
+    private final CaptchaImageResource captchaImageResource;
+    private String captchaText;
+    private String captchaPassword;
+    static private SecureRandom prng ;
+    {
+        try {
+           prng = SecureRandom.getInstance("SHA1PRNG");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();  
+        }
+    }
+
     @SpringBean
     UserDao userDao;
     public RegisterPage() {
-        final TUser user = new TUser();
-        final StatelessForm form = new StatelessForm("form", new CompoundPropertyModel(user));
-            form.add(new TextField("login"));
+        captchaPassword=Integer.toString(prng.nextInt(1000));
+        add(new FeedbackPanel("feedback"));
+        final IModel<TUser> newUserModel = new LoadableDetachableModel<TUser>() {
+            @Override
+            protected TUser load() {
+                return new TUser();
+            }
+        };
+        final StatelessForm form = new StatelessForm("form", new CompoundPropertyModel(newUserModel));
+
+            form.add(new TextField<String>("login"));
             form.add(new PasswordTextField("password"));
-            form.add(new TextField("profile.email"));
-            captchaImageResource = new CaptchaImageResource("123");
+            form.add(new TextField<String>("profile.email"));
+            captchaImageResource = new CaptchaImageResource(captchaPassword);
+
             form.add(new Image("captchaImage", captchaImageResource));
-            form.add(new TextField("text"));
+            form.add(new RequiredTextField<String>("captchaText",new PropertyModel(this, "captchaText")));;
 
 
             form.add(new Button("submit"){
                 @Override
                 public void onSubmit() {
-                    try {
-                        SecureRandom prng = SecureRandom.getInstance("SHA1PRNG");
-                        user.setActivationCode(""+prng.nextInt());
-                        userDao.create(user);
+                        if(captchaPassword.equals(captchaText)){
+                            TUser user=newUserModel.getObject();
+                            user.setActivationCode(""+prng.nextInt());
+                            userDao.create(user);
+                            //send activation email
+                        }else{
+                            error("Проверочный код введён неправильно");
+                        }
 
-                    } catch (NoSuchAlgorithmException e) {
-                        e.printStackTrace();
-                    }
 
                 }
             });
         add(form);
+    }
+
+    public String getCaptchaText() {
+        return captchaText;
+    }
+
+    public void setCaptchaText(String captchaText) {
+        this.captchaText = captchaText;
     }
 }
