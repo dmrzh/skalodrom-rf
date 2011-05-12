2f5ab375da76c38e645ceed8473f6373aa693768
Rzhevskiy Dmitry <dima@rzhevskiy.info>
2011-05-12 11:43:02 +0400

    i18n

--- src/main/java/ru/skalodrom_rf/dao/UserDao.java
+++ src/main/java/ru/skalodrom_rf/dao/UserDao.java
@@ -2,10 +2,12 @@ package ru.skalodrom_rf.dao;
 
 import net.sf.autodao.AutoDAO;
 import net.sf.autodao.Dao;
+import net.sf.autodao.Finder;
 import ru.skalodrom_rf.model.User;
 
 
 @AutoDAO
 public interface UserDao extends Dao<User, String> {
-  
+    @Finder(query = "select count(*) from User")
+    Long getUserCount();
 }
--- src/main/java/ru/skalodrom_rf/web/pages/BasePage.java
+++ src/main/java/ru/skalodrom_rf/web/pages/BasePage.java
@@ -4,9 +4,11 @@ import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import ru.skalodrom_rf.dao.UserDao;
 import ru.skalodrom_rf.web.components.ActivatePanel;
 import ru.skalodrom_rf.web.components.LoginPanel;
 import ru.skalodrom_rf.web.components.LogoutPanel;
@@ -16,6 +18,9 @@ import java.util.Calendar;
 
 /**.*/
 public class BasePage extends WebPage{
+
+    @SpringBean
+    private UserDao userDao;
 
     public BasePage() {
         init();        
@@ -27,6 +32,7 @@ public class BasePage extends WebPage{
 
     private void init() {
         add(new BookmarkablePageLink<Void>("indexLink", IndexPage.class));
+        add(new Label("users",userDao.getUserCount().toString()));
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(authentication!=null){
             final GrantedAuthority grantedAuthority = authentication.getAuthorities().iterator().next();//todo
@@ -44,9 +50,9 @@ public class BasePage extends WebPage{
     }
 
     private void addStatisticLabel() {
-        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL);
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, getSession().getLocale());
         final Calendar cal = Calendar.getInstance(getLocale());
-        cal.add(Calendar.DAY_OF_WEEK,1);
+        //cal.add(Calendar.DAY_OF_WEEK,1);
         final String s = dateFormat.format(cal.getTime());
         add(new Label("nextDate", s));
     }
