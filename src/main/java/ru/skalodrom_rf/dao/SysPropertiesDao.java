9efabca34e8235567da5074985d48aa7661d14f1
Rzhevskiy Dmitry <dima@rzhevskiy.info>
2011-05-21 00:02:02 +0400

    SysProperties added, Mapping corrected,content, robots, development configuration

---
+++ src/main/java/ru/skalodrom_rf/dao/SysPropertiesDao.java
@@ -0,0 +1,11 @@
+package ru.skalodrom_rf.dao;
+
+import net.sf.autodao.AutoDAO;
+import net.sf.autodao.Dao;
+import ru.skalodrom_rf.model.SysProperties;
+
+/**.*/
+@AutoDAO
+public interface SysPropertiesDao extends Dao<SysProperties,String> {
+
+}
